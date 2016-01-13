5249adb030011aa9e5c9a6404da48c7884a73863
Samuel Kaufman <samuel.c.kaufman@gmail.com>
2016-01-13 19:17:07 +0000

    committing stuff from cpan

---
+++ lib/Net/OAuth2.pm
@@ -0,0 +1,14 @@
+# Copyrights 2013-2015 by [Mark Overmeer].
+#  For other contributors see Changes.
+# See the manual pages for details on the licensing terms.
+# Pod stripped from pm file by OODoc 2.02.
+package Net::OAuth2;
+use vars '$VERSION';
+$VERSION = '0.62';
+
+
+use warnings;
+use strict;
+
+
+1;
