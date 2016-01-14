6303f18d2fd67867e74be31808e039d857ecc14e
Samuel Kaufman <samuel.c.kaufman@gmail.com>
2016-01-14 17:49:13 +0000

    stub for socialflows fork

---
+++ lib/SocialFlow/Net/OAuth2.pm
@@ -0,0 +1,16 @@
+package SocialFlow::Net::OAuth2;
+
+use strict;
+use warnings;
+
+=head1 SocialFlow::Net::OAuth2
+
+=head1 DESCRIPTION
+
+SocialFlow::Net::OAuth2 - version holder for SocialFlow's forked Net::OAuth2
+
+=cut
+
+our $VERSION = 0.001;
+
+1;
